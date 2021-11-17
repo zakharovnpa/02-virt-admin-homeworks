998b031c164f474e5044de0b8468b30c6142aae0
zakharovnpa <89311498+zakharovnpa@users.noreply.github.com>
2021-11-17 13:38:58 +0400

    Create start-vagrant-on-ubuntu-wsl.sh

---
+++ 05-virt-02-iaac/start-vagrant-on-ubuntu-wsl.sh
@@ -0,0 +1,129 @@
+Windows PowerShell
+(C) Корпорация Майкрософт (Microsoft Corporation). Все права защищены.
+
+Попробуйте новую кроссплатформенную оболочку PowerShell (https://aka.ms/pscore6)
+
+PS C:\Users\Sergey> wsl
+maestro@Sergey-PC:/mnt/c/Users/Sergey$ ls -lha
+total 217M
+drwxrwxrwx 1 maestro maestro  512 Nov 17 10:22 .
+dr-xr-xr-x 1 maestro maestro  512 Jan 21  2020 ..
+drwxrwxrwx 1 maestro maestro  512 Aug 18 16:48 3D Objects
+drwxrwxrwx 1 maestro maestro  512 Jul  3  2019 AppData
+lrwxrwxrwx 1 maestro maestro   35 Jul  3  2019 Application Data -> /mnt/c/Users/Sergey/AppData/Roaming
+drwxrwxrwx 1 maestro maestro  512 Jan 21  2016 Cisco Packet Tracer 6.0.1
+drwxrwxrwx 1 maestro maestro  512 Aug 18 16:48 Contacts
+lrwxrwxrwx 1 maestro maestro   63 Jul  3  2019 Cookies -> /mnt/c/Users/Sergey/AppData/Local/Microsoft/Windows/INetCookies
+drwxrwxrwx 1 maestro maestro  512 Sep 13 12:18 .dbus-keyrings
+drwxrwxrwx 1 maestro maestro  512 Nov 17 13:06 Desktop
+drwxrwxrwx 1 maestro maestro  512 Oct  2 10:07 Documents
+-rwxrwxrwx 1 maestro maestro    0 Sep 27 15:51 DocumentsVBoxManage_Help.txt
+drwxrwxrwx 1 maestro maestro  512 Nov 17 13:06 Downloads
+drwxrwxrwx 1 maestro maestro  512 Aug 18 16:48 Favorites
+drwxrwxrwx 1 maestro maestro  512 Jan 21  2016 .gimp-2.8
+drwxrwxrwx 1 maestro maestro  512 Aug 18 16:48 Links
+lrwxrwxrwx 1 maestro maestro   33 Jul  3  2019 Local Settings -> /mnt/c/Users/Sergey/AppData/Local
+drwxrwxrwx 1 maestro maestro  512 Jul 16  2018 MicrosoftEdgeBackups
+drwxrwxrwx 1 maestro maestro  512 Aug 18 16:48 Music
+lrwxrwxrwx 1 maestro maestro   71 Jul  3  2019 NetHood -> /mnt/c/Users/Sergey/AppData/Roaming/Microsoft/Windows/Network Shortcuts
+-rwxrwxrwx 1 maestro maestro  13M Nov 17 13:04 NTUSER.DAT
+-rwxrwxrwx 1 maestro maestro  64K Jul  3  2019 NTUSER.DAT{fd9a35db-49fe-11e9-aa2c-248a07783950}.TM.blf
+-rwxrwxrwx 1 maestro maestro 512K Jul  3  2019 NTUSER.DAT{fd9a35db-49fe-11e9-aa2c-248a07783950}.TMContainer00000000000000000001.regtrans-ms
+-rwxrwxrwx 1 maestro maestro 512K Jul  3  2019 NTUSER.DAT{fd9a35db-49fe-11e9-aa2c-248a07783950}.TMContainer00000000000000000002.regtrans-ms
+-rwxrwxrwx 1 maestro maestro 3.3M Jul  3  2019 ntuser.dat.LOG1
+-rwxrwxrwx 1 maestro maestro    0 Jul  3  2019 ntuser.dat.LOG2
+-rwxrwxrwx 1 maestro maestro   20 Jul  3  2019 ntuser.ini
+drwxrwxrwx 1 maestro maestro  512 Nov 17 13:06 OneDrive
+drwxrwxrwx 1 maestro maestro  512 Oct 16  2020 OpenVPN
+drwxrwxrwx 1 maestro maestro  512 Jul 20  2016 .oracle_jre_usage
+-rwxrwxrwx 1 maestro maestro  190 Aug 18  2016 .packettracer
+drwxrwxrwx 1 maestro maestro  512 Sep 17 18:48 Pictures
+lrwxrwxrwx 1 maestro maestro   71 Jul  3  2019 PrintHood -> /mnt/c/Users/Sergey/AppData/Roaming/Microsoft/Windows/Printer Shortcuts
+lrwxrwxrwx 1 maestro maestro   60 Jul  3  2019 Recent -> /mnt/c/Users/Sergey/AppData/Roaming/Microsoft/Windows/Recent
+drwxrwxrwx 1 maestro maestro  512 Aug 18 16:48 Saved Games
+drwxrwxrwx 1 maestro maestro  512 Aug 18 16:48 Searches
+lrwxrwxrwx 1 maestro maestro   60 Jul  3  2019 SendTo -> /mnt/c/Users/Sergey/AppData/Roaming/Microsoft/Windows/SendTo
+drwxrwxrwx 1 maestro maestro  512 Sep 27 14:52 .ssh
+drwxrwxrwx 1 maestro maestro  512 Jan 21  2016 .swt
+drwxrwxrwx 1 maestro maestro  512 Jan 21  2016 .thumbnails
+drwxrwxrwx 1 maestro maestro  512 Nov 10  2016 Tracing
+-rwxrwxrwx 1 maestro maestro 200M Nov 16 20:49 Ubuntu.zip
+drwxrwxrwx 1 maestro maestro  512 Nov 17 10:30 .vagrant.d
+drwxrwxrwx 1 maestro maestro  512 Nov 17 10:22 Vagrant-project
+drwxrwxrwx 1 maestro maestro  512 Aug 18 16:48 Videos
+drwxrwxrwx 1 maestro maestro  512 Nov 17 13:21 .VirtualBox
+drwxrwxrwx 1 maestro maestro  512 Oct 30  2015 VirtualBox VMs
+drwxrwxrwx 1 maestro maestro  512 May  1  2021 xod
+lrwxrwxrwx 1 maestro maestro   64 Jul  3  2019 главное меню -> /mnt/c/Users/Sergey/AppData/Roaming/Microsoft/Windows/Start Menu
+lrwxrwxrwx 1 maestro maestro   29 Jul  3  2019 Мои документы -> /mnt/c/Users/Sergey/Documents
+lrwxrwxrwx 1 maestro maestro   63 Jul  3  2019 Шаблоны -> /mnt/c/Users/Sergey/AppData/Roaming/Microsoft/Windows/Templates
+maestro@Sergey-PC:/mnt/c/Users/Sergey$ cd Vagrant-project/
+maestro@Sergey-PC:/mnt/c/Users/Sergey/Vagrant-project$ cd Ubuntu-1/
+maestro@Sergey-PC:/mnt/c/Users/Sergey/Vagrant-project/Ubuntu-1$
+maestro@Sergey-PC:/mnt/c/Users/Sergey/Vagrant-project/Ubuntu-1$ vagrant status
+Vagrant failed to initialize at a very early stage:
+
+Vagrant is unable to use the VirtualBox provider from the Windows Subsystem for
+Linux without access to the Windows environment. Enabling this access must be
+done with caution and an understanding of the implications. For more information
+on enabling Windows access and using VirtualBox from the Windows Subsystem for
+Linux, please refer to the Vagrant documentation:
+
+  https://www.vagrantup.com/docs/other/wsl.html
+maestro@Sergey-PC:/mnt/c/Users/Sergey/Vagrant-project/Ubuntu-1$ ls -lha
+total 4.0K
+drwxrwxrwx 1 maestro maestro  512 Nov 17 10:30 .
+drwxrwxrwx 1 maestro maestro  512 Nov 17 10:22 ..
+drwxrwxrwx 1 maestro maestro  512 Nov 17 10:30 .vagrant
+-rwxrwxrwx 1 maestro maestro 3.0K Nov 17 10:29 Vagrantfile
+maestro@Sergey-PC:/mnt/c/Users/Sergey/Vagrant-project/Ubuntu-1$
+maestro@Sergey-PC:/mnt/c/Users/Sergey/Vagrant-project/Ubuntu-1$ vagrant up
+Vagrant failed to initialize at a very early stage:
+
+Vagrant is unable to use the VirtualBox provider from the Windows Subsystem for
+Linux without access to the Windows environment. Enabling this access must be
+done with caution and an understanding of the implications. For more information
+on enabling Windows access and using VirtualBox from the Windows Subsystem for
+Linux, please refer to the Vagrant documentation:
+
+  https://www.vagrantup.com/docs/other/wsl.html
+maestro@Sergey-PC:/mnt/c/Users/Sergey/Vagrant-project/Ubuntu-1$ export VAGRANT_WSL_ENABLE_WINDOWS_ACCESS="1"
+maestro@Sergey-PC:/mnt/c/Users/Sergey/Vagrant-project/Ubuntu-1$
+maestro@Sergey-PC:/mnt/c/Users/Sergey/Vagrant-project/Ubuntu-1$ vagrant status
+Current machine states:
+
+default                   poweroff (virtualbox)
+
+The VM is powered off. To restart the VM, simply run `vagrant up`
+maestro@Sergey-PC:/mnt/c/Users/Sergey/Vagrant-project/Ubuntu-1$
+maestro@Sergey-PC:/mnt/c/Users/Sergey/Vagrant-project/Ubuntu-1$ vagrant up
+Bringing machine 'default' up with 'virtualbox' provider...
+==> default: Checking if box 'bento/ubuntu-20.04' version '202107.28.0' is up to date...
+==> default: Clearing any previously set forwarded ports...
+==> default: Fixed port collision for 22 => 2222. Now on port 2200.
+==> default: Clearing any previously set network interfaces...
+==> default: Preparing network interfaces based on configuration...
+    default: Adapter 1: nat
+==> default: Forwarding ports...
+    default: 22 (guest) => 2200 (host) (adapter 1)
+==> default: Booting VM...
+==> default: Waiting for machine to boot. This may take a few minutes...
+    default: SSH address: 127.0.0.1:2200
+    default: SSH username: vagrant
+    default: SSH auth method: private key
+    default:
+    default: Vagrant insecure key detected. Vagrant will automatically replace
+    default: this with a newly generated keypair for better security.
+    default:
+    default: Inserting generated public key within guest...
+    default: Removing insecure key from the guest if it's present...
+    default: Key inserted! Disconnecting and reconnecting using new SSH key...
+==> default: Machine booted and ready!
+==> default: Checking for guest additions in VM...
+==> default: Mounting shared folders...
+    default: /vagrant => /mnt/c/Users/Sergey/Vagrant-project/Ubuntu-1
+maestro@Sergey-PC:/mnt/c/Users/Sergey/Vagrant-project/Ubuntu-1$
+maestro@Sergey-PC:/mnt/c/Users/Sergey/Vagrant-project/Ubuntu-1$
+maestro@Sergey-PC:/mnt/c/Users/Sergey/Vagrant-project/Ubuntu-1$
+maestro@Sergey-PC:/mnt/c/Users/Sergey/Vagrant-project/Ubuntu-1$
+maestro@Sergey-PC:/mnt/c/Users/Sergey/Vagrant-project/Ubuntu-1$
